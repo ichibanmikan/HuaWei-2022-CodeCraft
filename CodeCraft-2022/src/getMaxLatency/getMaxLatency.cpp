c076bf8f14c38921f841532baed26a32d0b7d573
ichibanmikan <ichibanmikan@hnu.edu.cn>
2022-03-19 21:52:33 +0800

    读入最大QOS限制

---
+++ CodeCraft-2022/src/getMaxLatency/getMaxLatency.cpp
@@ -0,0 +1,15 @@
+#include "getMaxLatency.h"
+
+using std::ifstream;
+using std::string;
+
+void getMaxLatency(){
+    ifstream infile("/mnt/d/OneDrive - hnu.edu.cn/mylearn/HWSC/线下调试数据/线下调试数据/data/config.ini");
+    // ifstream infile("/data/config.ini");
+    string lineHead;
+    string lineNext;
+    getline(infile, lineHead);
+    getline(infile, lineNext);
+    string str=lineNext.substr(15);
+    LATENCY_MAX=stoi(str);
+}
---
+++ CodeCraft-2022/src/getMaxLatency/getMaxLatency.h
@@ -0,0 +1,10 @@
+#ifndef __GET_MAXLATENCYH_
+#define __GET_MAXLATENCYH_
+#include <fstream>
+#include <string>
+// #include "../lib/testHelp.h"
+
+void getMaxLatency();
+int LATENCY_MAX;
+
+#endif
